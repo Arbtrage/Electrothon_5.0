d9642e0e3c8d872f82f66902c3f404f25d872858
Sayantan <sayantann2002@gmail.com>
2023-04-08 00:40:30 +0530

    Added login and signUp backend

--- server/index.js
+++ server/index.js
@@ -2,15 +2,21 @@ const express=require('express');
 const app=express();
 require('dotenv').config();
 const morgan=require('morgan');
+const route=require('./routes/auth.route');
+const connectDB=require('./services/connectDb');
+
+app.use(express.json());
 
 
+connectDB();
 const port=process.env.PORT || 5000;
 
-// app.use(morgan);
+app.use('/api',route);
+app.use(morgan('dev'));
 
-app.get('/',(req,res)=>{
-    res.status(200).json({message:"Server running successfully...."});
-})
+// app.get('/',(req,res)=>{
+//     res.status(200).json({message:"Server running successfully...."});
+// })
 
 
 app.listen(port,()=>{
---
+++ server/models/user.model.js
@@ -0,0 +1,54 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+const bcrypt=require('bcrypt');
+
+const userSchema=new Schema({
+    userName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        lowercase:true,
+    },
+    profilePicture:{
+      type:String,
+      required:false
+    },
+    password:{
+        type:String,
+        default:"",
+    }
+})
+
+
+userSchema.pre('save',async function(next){
+    if(this.password===""){
+        return "";
+    }
+    try {
+        if (this.isNew) {
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(this.password, salt)
+            this.password = hashedPassword
+          }
+        next();
+    } catch (error) {
+        next(error);
+    }
+})
+
+
+userSchema.methods.isValidPassword = async function (password) {
+    
+    try {
+      return await bcrypt.compare(password, this.password)
+    } catch (error) {
+      throw error
+    }
+  }
+
+const User=mongoose.model('user',userSchema);
+
+module.exports=User;
