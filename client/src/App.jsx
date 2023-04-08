d3cb47bf9fa5fffeee8285b012ac1fa349ba5762
Udit Gupta <udgupta33@gmail.com>
2023-04-08 15:24:06 +0530

    Added auth pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Myprofile from './pages/MyProfile';
-
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
               <Routes>
                 <Route path="/" element={(<Home />)} />
                 <Route path="/myprofile" element={(<Myprofile /> )} />
+                <Route path="/login" element = {(<Login />)} />
+                <Route path="/signup" element = {(<Signup />)} />
               </Routes>
       </BrowserRouter>
     </div>
